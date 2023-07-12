5c00718e027f93d3e7bc283db1f2d283299009f3
Adrian Castillo <david.marcanao07@gmail.com>
2023-07-12 09:27:56 -0300

    upload

---
+++ Exercicios_global/Exercicios(Python)/Ejercicio3.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua media
+
+primera_nota = int(input('Informe la primera nota: '))
+segunda_nota = int(input('Informe la segunda nota: '))
+
+media = (primera_nota + segunda_nota) / 2
+
+print(f'La media del alumno es : {media}')
