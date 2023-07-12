9429872e7261f0bfcc9dcff4c0e2942f87da3abb
Adrian Castillo <david.marcanao07@gmail.com>
2023-07-12 09:12:10 -0300

    upload

---
+++ Exercicios_global/Exercicios(Python)/Ejercicio1.py
@@ -0,0 +1,8 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+
+numero = int(input('Informa un numero: '))
+
+sucesor = numero + 1
+antecesor = numero - 1
+
+print(f''' El numero sucesor es {sucesor} y el numero antecesor {antecesor}''')
