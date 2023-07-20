8f673e375a7f9b6dcb25ff296652b0fef29809f4
Adrian Castillo <david.marcanao07@gmail.com>
2023-07-20 10:54:02 -0300

    UPLOAD

---
+++ Exercicios_global/Exercicios(Python)/Ejercicio7.py
@@ -0,0 +1,10 @@
+# Ejercicio 3: Factorial de un número
+# Escribe una función que tome un número entero como entrada y devuelva su factorial.
+import math
+
+def factorial(numero):
+
+    # numero = 0
+    return math.factorial(numero)
+
+print(factorial(20))
